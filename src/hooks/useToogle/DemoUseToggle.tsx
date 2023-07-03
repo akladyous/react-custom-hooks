2a4ca00e7739429df211e483ac7f9a5032989058
Boula Akladyous <akladyous@gmail.com>
2023-07-03 18:07:39 -0400

    Untracked files on react-custom-hooks

---
+++ src/hooks/useToogle/DemoUseToggle.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import useToggle from './useToggle';
+import { Switch } from '../../components/ui/switch';
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ');
+}
+
+export default function DemoUseToggle() {
+  const [on, toggle] = useToggle(false);
+
+  return (
+    <main>
+      <h4>Use Toggle Hook</h4>
+
+      <Switch checked={on as boolean} />
+    </main>
+  );
+}
--- src/hooks/useToogle/useToggle.tsx
+++ src/hooks/useToogle/useToggle.tsx
@@ -1,9 +1,9 @@
 import { useCallback, useState } from 'react';
 
-export default function useToggle(initialValue = false) {
-  const [value, setValue] = useState(initialValue);
+export default function useToggle(initialValue: boolean = false) {
+  const [value, setValue] = useState<boolean>(initialValue);
 
-  const handleToggle = useCallback((value) => {
+  const handleToggle = useCallback((value: boolean): void => {
     if (typeof value === 'boolean') {
       return setValue(value);
     }
