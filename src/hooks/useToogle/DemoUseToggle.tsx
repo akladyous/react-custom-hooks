af7a556c31f19f3fd735b557c9a8dcd31c689929
Boula Akladyous <akladyous@gmail.com>
2023-07-04 15:03:27 -0400

    Untracked files on react-custom-hooks

--- src/hooks/useToogle/DemoUseToggle.tsx
+++ src/hooks/useToogle/DemoUseToggle.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import useToggle from './useToggle';
-import { Switch } from '../../components/ui/switch';
 
+import { Switch } from '@/components/ui/switch';
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
 }
