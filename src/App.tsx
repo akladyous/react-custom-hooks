83de794983d6f5f1be49d3259f05f2273a6af560
Boula Akladyous <akladyous@gmail.com>
2023-07-06 18:50:53 -0400

    Untracked files on react-custom-hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import DemoUseToggle from './hooks/useToogle/DemoUseToggle';
 import DemouseOnlineStatus from './hooks/useOnlineStatus/DemouseOnlineStatus';
+import DemoUseHover from './hooks/useHover/DemoUseHover';
 
 function App() {
   return (
-    <main>
+    <main className='w-4/5 border m-5 p-2 min-h-screen mx-auto'>
       <DemoUseToggle />
       <DemouseOnlineStatus />
+      <DemoUseHover />
     </main>
   );
 }
--- src/hooks/useHover/DemoUseHover.tsx
+++ src/hooks/useHover/DemoUseHover.tsx
@@ -6,8 +6,14 @@ const DemoUseHover: React.FC = () => {
   const isHovered = useHover(elementRef);
 
   return (
-    <div ref={elementRef}>
-      <h2>{isHovered ? 'Hovered' : 'Not Hovered'}</h2>
+    <div ref={elementRef} className=' bg-slate-50 w-3/4 mx-auto my-3 p-3'>
+      <h3>{isHovered ? 'Hovered' : 'Not Hovered'}</h3>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda,
+        repudiandae cumque molestiae veniam optio ab, saepe quas illum sed
+        facilis laboriosam eligendi nulla! Iure at dolorem, amet pariatur unde
+        culpa?
+      </p>
     </div>
   );
 };
